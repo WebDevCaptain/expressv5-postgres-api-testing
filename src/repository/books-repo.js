9e274f4d3cbe24e580e99842222f371813f2ccb6
Shreyash <hello@webdevcaptain.com>
2025-01-18 08:46:43 +0530

    Finish basic CRUD APIs for books. Needs data sanitization and validation to prevent security issues

---
+++ src/repository/books-repo.js
@@ -0,0 +1,63 @@
+const pool = require("../pool");
+const camelcase = require("camelcase").default;
+
+const toCamelCase = (rows) => {
+  return rows.map((row) => {
+    const camelCasedObj = {};
+
+    for (let key in row) {
+      camelCasedObj[camelcase(key)] = row[key];
+    }
+
+    return camelCasedObj;
+  });
+};
+
+class BookRepo {
+  static async find() {
+    const { rows } = await pool.query("SELECT * FROM books;");
+    return toCamelCase(rows);
+  }
+
+  static async findById(id) {
+    const { rows } = await pool.query("SELECT * FROM books WHERE id = $1;", [
+      id,
+    ]);
+
+    return toCamelCase(rows)[0];
+  }
+
+  static async insert(title, author) {
+    const { rows } = await pool.query(
+      "INSERT INTO books(title, author) VALUES ($1, $2) RETURNING *;",
+      [title, author]
+    );
+    return toCamelCase(rows)[0];
+  }
+
+  static async update({ id, title, author, price }) {
+    const { rows } = await pool.query(
+      "UPDATE books SET title = $1, author = $2, price = $3 WHERE id = $4 RETURNING *;",
+      [title, author, price, id]
+    );
+
+    return toCamelCase(rows)[0];
+  }
+
+  static async delete(id) {
+    const { rows } = await pool.query(
+      "DELETE FROM books WHERE id = $1 RETURNING *;",
+      [id]
+    );
+
+    return toCamelCase(rows)[0];
+  }
+
+  static async count() {
+    const { rows } = await pool.query("SELECT COUNT(*) FROM books;");
+
+    return parseInt(rows[0].count);
+  }
+}
+
+module.exports = BookRepo;
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,19 +1,65 @@
 const express = require("express");
+const BookRepo = require("../repository/books-repo");
 const router = express.Router();
 
 router.get("/books", async (req, res) => {
-  res.status(200).send({
-    title: "Staff Engineer Handbook",
-    price: 2900,
+  const books = await BookRepo.find();
+  const count = await BookRepo.count();
+
+  res.send({
+    books,
+    count,
   });
 });
 
-router.post("/books", async (req, res) => {});
+router.post("/books", async (req, res) => {
+  const { title, author } = req.body;
 
-router.get("/books/:id", async (req, res) => {});
+  const book = await BookRepo.insert(title, author);
+  res.send(book);
+});
 
-router.patch("/books/:id", async (req, res) => {});
+router.get("/books/:id", async (req, res) => {
+  const { id } = req.params;
 
-router.delete("/books/:id", async (req, res) => {});
+  const book = await BookRepo.findById(id);
+
+  if (book) {
+    res.send(book);
+  } else {
+    res.status(404).json({
+      error: `Book with ID = ${id} doesn't exist.`,
+    });
+  }
+});
+
+router.put("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, author, price } = req.body;
+
+  const book = await BookRepo.update({ id, title, author, price });
+
+  if (book) {
+    res.send(book);
+  } else {
+    res.status(404).json({
+      error: `Book with ID = ${id} cannot be updated.`,
+    });
+  }
+});
+
+router.delete("/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const book = await BookRepo.delete(id);
+
+  if (book) {
+    res.send(book);
+  } else {
+    res.status(404).json({
+      error: `Book with ID = ${id} cannot be deleted.`,
+    });
+  }
+});
 
 module.exports = router;
