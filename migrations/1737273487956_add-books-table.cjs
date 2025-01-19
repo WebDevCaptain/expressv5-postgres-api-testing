cf45f2450ff466b0e02063f2f80c91e014a377a9
Shreyash <hello@webdevcaptain.com>
2025-01-19 13:31:06 +0530

    Regenerate migrations file to use commonjs extension

--- migrations/1737273487956_add-books-table.cjs
+++ migrations/1737273487956_add-books-table.cjs
@@ -1,14 +1,14 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-export const shorthands = undefined;
+exports.shorthands = undefined;
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {
+exports.up = (pgm) => {
   pgm.sql(`
         CREATE TABLE books(
             id SERIAL PRIMARY KEY,
@@ -25,8 +25,8 @@ export const up = (pgm) => {
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {
+exports.down = (pgm) => {
   pgm.sql(`
-       DROP TABLE books; 
-    `);
+        DROP TABLE books; 
+     `);
 };
